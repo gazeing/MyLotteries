917434d71fbc020ef6b3baa8bb7da7fa99720599
Loren Chen <loren-chen@hotmail.com>
2014-02-26 02:35:45 +1100

    Update the framework library.

---
+++ src/com/sydney/mylotteries/input/InputImpl.java
@@ -0,0 +1,53 @@
+package com.sydney.mylotteries.input;
+
+import com.google.gson.Gson;
+
+public abstract class InputImpl {
+	/**
+	 * Interface to get the type of the implementation. 
+	 * @return
+	 */
+	public abstract int getType();
+	
+	/**
+	 * Interface to start a new Input delegate
+	 * @param aParam Gson param to initialize the delegate
+	 * @return
+	 */
+	public abstract boolean doStart(Gson aParam);
+	
+	/**
+	 * API to cancel the input.
+	 */
+	public final void doCancel() {
+		if( null != mDelegate ) {
+			mDelegate.onCancel();
+		}
+	}
+	
+	/**
+	 * notify result to delegate
+	 * @param aResult
+	 */
+	public final void notifyResult(Gson aResult) {
+		if( null != mDelegate ) {
+			mDelegate.onResult(aResult);
+		}
+		
+		// Reset the delegate to null after notification.
+		mDelegate = null;
+	}
+	
+	/**
+	 * Set the delegate instance.
+	 * @param aDelegate
+	 */
+	public final void setDelegate(InputDelegate aDelegate) {
+		mDelegate = aDelegate;
+	}
+	
+	/**
+	 * Member instances.
+	 */
+	private InputDelegate mDelegate;
+}
